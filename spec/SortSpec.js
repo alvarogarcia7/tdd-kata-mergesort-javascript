fec667f61b302ebf9203b6eb15a1471a291ef802
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-04-26 21:10:29 +0200

    red: regression test

--- spec/SortSpec.js
+++ spec/SortSpec.js
@@ -28,4 +28,17 @@ describe("Merge sort", function(){
 	it("should sort any five numbers", function(){
 		expect(sorter.sort([0,10,9,8,1])).toEqual([0,1,8,9,10]);
 	});
+
+	it("should sort any size of  numbers", function(){
+		expect(sorter.sort([10,9,8,4,5,6,7,1,2,3])).toEqual([1,2,3,4,5,6,7,8,9,10]);
+	});
+
+	it("should not sort an empty array", function(){
+		expect(sorter.sort([])).toEqual([]);
+	});
+
+	it("should not sort a null array", function(){
+		expect(sorter.sort(null)).toEqual(null);
+	});
+
 });
