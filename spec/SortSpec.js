67ca9d85a9820363c2a673f2edd4fa062799700b
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-04-26 20:28:35 +0200

    red: Merge sort should sort any two numbers.

--- spec/SortSpec.js
+++ spec/SortSpec.js
@@ -2,4 +2,8 @@ describe("Merge sort", function(){
 	it("should exist as an object", function(){
 		expect(new MergeSort()).toBeTruthy();
 	});
+
+	it("should sort any two numbers", function(){
+		expect(new MergeSort().sort([1,0])).toEqual([0,1]);
+	});
 });
