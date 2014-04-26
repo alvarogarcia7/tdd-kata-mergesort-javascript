6b4984f9253e014982e19fceec3416b457d92f66
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-04-26 20:32:12 +0200

    refactor: extract initialization to beforeEach

--- spec/SortSpec.js
+++ spec/SortSpec.js
@@ -1,9 +1,15 @@
 describe("Merge sort", function(){
+	var sorter;
+
+	beforeEach(function(){
+		sorter = new MergeSort();
+	});
+
 	it("should exist as an object", function(){
-		expect(new MergeSort()).toBeTruthy();
+		expect(sorter).toBeTruthy();
 	});
 
 	it("should sort any two numbers", function(){
-		expect(new MergeSort().sort([1,0])).toEqual([0,1]);
+		expect(sorter.sort([1,0])).toEqual([0,1]);
 	});
 });
