14b470416f2a39e22bb2cb975f8f6a69fb45ec23
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-04-26 20:57:37 +0200

    green

--- src/MergeSort.js
+++ src/MergeSort.js
@@ -3,11 +3,25 @@ var MergeSort = function(){
 };
 
 MergeSort.prototype.sort = function(list) {
-	if(2 == list.length){
+	if(1 == list.length){
+		return list;
+	} else if(2 == list.length){
 		if(list[0] > list[1]){
 			return this.swap(list, 0, 1);
 		}
 		return list;
+	} else  {
+		var firstHalf = [], secondHalf = [], result = [];
+
+		firstHalf.push(list[0]);
+		firstHalf.push(list[1]);
+		firstHalf = this.sort(firstHalf);
+
+		secondHalf.push(list[2]);
+		secondHalf = this.sort(secondHalf);
+
+		this.merge(result, firstHalf, secondHalf);
+		return result;
 	}
 };
 
@@ -16,4 +30,24 @@ MergeSort.prototype.swap = function(list, positionA, positionB) {
 	list[positionB] = list[positionA];
 	list[positionA] = temporary;
 	return list;	
+};
+
+MergeSort.prototype.merge = function(result, firstHalf, secondHalf) {
+	var i = 0, j = 0;
+	while(i < firstHalf.length && j < secondHalf.length){
+		if (firstHalf[i] < secondHalf[j]){
+			result.push(firstHalf[i]);
+			i++;
+		} else {
+			result.push(secondHalf[j]);
+			j++;
+		}
+	}
+	if(i != firstHalf.length || j != secondHalf.length){
+		while(j< secondHalf.length){
+			result.push(secondHalf[j]);
+			j++;
+		}
+	}
+	return result;
 };
