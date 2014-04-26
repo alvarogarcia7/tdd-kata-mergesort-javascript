5f75f23cb3bdfc795bdad60cb8abd1a6c1783c04
Alvaro Garcia <alvaro.garcia@newshore.es>
2014-04-26 20:31:15 +0200

    green

--- src/MergeSort.js
+++ src/MergeSort.js
@@ -1,3 +1,16 @@
 var MergeSort = function(){
 
 };
+
+MergeSort.prototype.sort = function(list) {
+	if(2 == list.length){
+		return this.swap(list, 0, 1);
+	}
+};
+
+MergeSort.prototype.swap = function(list, positionA, positionB) {
+	var temporary = list[positionB];
+	list[positionB] = list[positionA];
+	list[positionA] = temporary;
+	return list;	
+};
